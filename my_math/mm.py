a870612d8b64ca5644a566c2b2cdb594078d3dae
rkpust <rezaul.cse.pust17@gmail.com>
2025-01-20 13:43:13 +0600

    added and tested add function

--- my_math/mm.py
+++ my_math/mm.py
@@ -1,2 +1,11 @@
 # Constant
 PI=3.141592653589793
+
+# Arithmetic
+def add(*args):
+    sum = 0
+
+    for number in args:
+        sum += number
+
+    return sum
--- test.py
+++ test.py
@@ -6,6 +6,13 @@ class TestMyMath(unittest.TestCase):
     def test_PI(self):
         self.assertEqual(mm.PI, 3.141592653589793)
 
+    # Arithmetic
+    def test_add(self):
+        self.assertEqual(mm.add(5), 5)
+        self.assertEqual(mm.add(-1, 1), 0)
+        self.assertEqual(mm.add(1, 2, 4), 7)
+        self.assertEqual(mm.add(1, 5, -4, 8), 10)
+
 
 if __name__ == "__main__":
     unittest.main()
